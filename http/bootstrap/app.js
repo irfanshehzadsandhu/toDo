601e7221f7701dbee5c327a89038b171978a438a
musti1 <mustafi51992@gmail.com>
2020-02-21 00:34:43 +0500

    refactor code for route paths

--- http/bootstrap/app.js
+++ http/bootstrap/app.js
@@ -3,23 +3,13 @@ const express = require("express");
 // initialize our express app
 const app = express();
 const bodyParser = require("body-parser");
-const homePageRoute = require("../routes/homepage.route");
-const todosRoute = require("../routes/todo.route");
-const usersRoute = require("../routes/user.route");
 const db = require("../../services/connect.database");
+const routes = require("../routes");
+// Verify this is right approach
+routes(app);
+//connect to database
 db();
 //set view engine
 app.set("view engine", "jade");
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get("/", homePageRoute.homepage);
-app.use("/api/v1/todos", todosRoute);
-app.use("/api/v1/users", usersRoute);
-
-app.set("view engine", "jade");
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get("/", homePageRoute.homepage);
-app.use("/api/v1/todos", todosRoute);
-app.use("/api/v1/users", usersRoute);
 module.exports = app;
---
+++ http/routes/index.js
@@ -0,0 +1,9 @@
+const app = require("../bootstrap/app");
+const homePageRoute = require("./homepage.route");
+const todosRoute = require("./todo.route");
+const usersRoute = require("./user.route");
+module.exports = app => {
+  app.get("/", homePageRoute.homepage);
+  app.use("/api/v1/todos", todosRoute);
+  app.use("/api/v1/users", usersRoute);
+};
