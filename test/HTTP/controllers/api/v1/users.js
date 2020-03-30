136cb042597c4b7575f82e04d623a806fab7a6e3
Irfan Shahzad <irfanshehzadsandhu@gmail.com>
2020-03-30 05:15:53 +0500

    understanding spies,stubs and mocks

---
+++ test/HTTP/controllers/api/v1/users.js
@@ -0,0 +1,29 @@
+const expect = require("chai").expect;
+const sinon = require("sinon");
+const faker = require("faker");
+const usersController = require("../../../../../HTTP/controllers/api/v1/users");
+
+describe("Users Controller.", () => {
+    it("Should send status 200 on user registration successfully.", async () => {
+        const req = {
+            body: {
+                name: faker.name.findName(),
+                email: faker.internet.email(),
+                password: faker.internet.password()
+            }
+        }
+
+        const res = Object.create({});
+        res.status = function (status) {
+            console.log("Controller sent following status:", status);
+            return this;
+        }
+
+        // res.json = function (obj) {
+        //     console.log("Response in json is:", sinon.spy());
+        // }
+        res.json = sinon.spy()
+        await usersController.create(req, res);
+        expect(res.json.calledOnce).to.be.true;
+    });
+});
