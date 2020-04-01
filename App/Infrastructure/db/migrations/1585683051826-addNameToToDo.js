1a910b28835185fdd06ec3bd257fa60e7c5ceb2a
Irfan Shahzad <irfanshehzadsandhu@gmail.com>
2020-04-01 17:42:23 +0500

    migration code refactoring

--- App/Infrastructure/db/migrations/1585683051826-addNameToToDo.js
+++ App/Infrastructure/db/migrations/1585683051826-addNameToToDo.js
@@ -3,16 +3,14 @@ const toDo = require("../../models/todo");
  * Make any changes you need to make to the database here
  */
 async function up() {
-  const result = toDo.update({}, { name: "New field name is added" });
-  console.log(result);
+  console.log(await this('ToDo').updateMany({}, { name: "New field name is added" }));
 }
 
 /**
  * Make any changes that UNDO the up function side effects here (if possible)
  */
 async function down() {
-  const result = toDo.update({}, { '$unset': { name: true } });
-  console.log(result);
+  console.log(await this('ToDo').updateMany({}, { '$unset': { name: true } }));
 }
 
 module.exports = { up, down };
