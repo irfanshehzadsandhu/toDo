0e2599200f2794d2b0ab7b03535b7829423295b3
Irfan Shahzad <irfanshehzadsandhu@gmail.com>
2020-04-09 22:55:11 +0500

    remove common-env plugin for heroku

--- App/Infrastructure/config/database.js
+++ App/Infrastructure/config/database.js
@@ -1,12 +1,5 @@
-require('dotenv').config()
-const env = require("common-env")();
-module.exports = env.getOrElseAll({
-  host: {
-    $default: "",
-    $aliases: ["DB", "DATABASE_URL"] //will pick from .env file. If DB and DBHOST are not present in .env file then it will pick default value
-  },
-  driver: {
-    $default: "",
-    $aliases: ["DATABASEDRIVER"]
-  }
-});
+require('dotenv').config();
+module.exports = {
+  host: process.env.DATABASE_URL,
+  driver: process.env.DATABASEDRIVER
+};
