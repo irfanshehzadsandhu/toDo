7cbe00e93a13146d5628bc04171bb93a272d950e
Irfan Shahzad <irfanshehzadsandhu@gmail.com>
2020-04-09 20:55:01 +0500

    environmental variable of db

--- App/Infrastructure/config/database.js
+++ App/Infrastructure/config/database.js
@@ -3,7 +3,7 @@ const env = require("common-env")();
 module.exports = env.getOrElseAll({
   host: {
     $default: "",
-    $aliases: ["DB", "DBHOST"] //will pick from .env file. If DB and DBHOST are not present in .env file then it will pick default value
+    $aliases: ["DB", "DATABASE_URL"] //will pick from .env file. If DB and DBHOST are not present in .env file then it will pick default value
   },
   driver: {
     $default: "",
