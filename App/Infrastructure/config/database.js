3460bb0253b6300202569ac2414d9b4424e7c949
Irfan Shahzad <irfanshehzadsandhu@gmail.com>
2020-04-09 20:41:24 +0500

    set default values

--- App/Infrastructure/config/database.js
+++ App/Infrastructure/config/database.js
@@ -2,11 +2,11 @@ require('dotenv').config()
 const env = require("common-env")();
 module.exports = env.getOrElseAll({
   host: {
-    $default: "mongodb://localhost:27017/todo_application",
+    $default: "",
     $aliases: ["DB", "DBHOST"] //will pick from .env file. If DB and DBHOST are not present in .env file then it will pick default value
   },
   driver: {
-    $default: "mongoose",
+    $default: "",
     $aliases: ["DATABASEDRIVER"]
   }
 });
